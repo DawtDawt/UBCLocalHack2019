403cc078bce6e251c918dee5aba98704cd72cbc6
jusung <jusunglee1459@gmail.com>
2019-11-30 13:41:34 -0800

    login html

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom'
 import Homepage from "./components/Homepage";
+import Login from "./components/Login";
 
 function App() {
   return (
       <React.Fragment>
         <Switch>
           <Route exact path={"/"} component={Homepage}/>
-          {/*<Route exact path={"/login"} component={Login}/>*/}
+          <Route exact path={"/login"} component={Login}/>
           {/*<Route exact path={"/login"} component={SignUp}/>*/}
         </Switch>
       </React.Fragment>
