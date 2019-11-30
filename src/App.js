0085eec922fb5be91aab416a1f92198b79794768
Eric <erictomokigoto@gmail.com>
2019-11-30 13:53:46 -0800

    created signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom'
 import Homepage from "./components/Homepage";
+import Signup from "./components/Signup";
 import Login from "./components/Login";
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
       <React.Fragment>
         <Switch>
           <Route exact path={"/"} component={Homepage}/>
+          <Route exact path={"/Signup"} component={Signup}/>
           <Route exact path={"/login"} component={Login}/>
-          {/*<Route exact path={"/login"} component={SignUp}/>*/}
         </Switch>
       </React.Fragment>
   );
