0e5f1680c74a5e20c0f76f773909bacfe3c93d8d
T.J. Wu <tjwu24@gmail.com>
2019-11-30 12:59:19 -0800

    init frontend

---
+++ src/components/Homepage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {Button} from "react-bootstrap";
+
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {vehicles: []};
+    }
+
+    render() {
+        const btnStyle = {
+            margin: "10px",
+            width: "120px"
+        };
+
+        return (
+            <div className="wrapper">
+                <div>
+                    <h1>I am a...</h1>
+                </div>
+                <div>
+                    <Link to="/login">
+                        <Button variant={"primary"} size={"lg"} style={btnStyle}>Login</Button>
+                    </Link>
+                    <Link to="/signup">
+                        <Button variant={"primary"} size={"lg"} style={btnStyle}>Sign Up</Button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Homepage;
