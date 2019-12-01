16c2e7da3ed340cebd233749ad19f43c93d41d74
jusung <jusunglee1459@gmail.com>
2019-11-30 17:24:14 -0800

    maps

--- src/components/maps.js
+++ src/components/maps.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>
 
-class Maps extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {vehicles: []};
-    }
-
-    render() {
-        const btnStyle = {
-            margin: "10px",
-            width: "120px"
-        };
-
+class Maps extends Component {
+    static defaultProps = {
+        center: {
+          lat: 49.2581,
+          lng: -123.2467873
+        },
+        zoom: 11
+      };
+    
+      render() {
         return (
-            
-                <text>Hello</text>
-        )
-    }
+        <div style={{height:'400px', width: '100%'}}>
+          
+          <div style={{ height: '100%', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: "AIzaSyBx7ozH-v1C-BxgCUPgh84DGMtcsAZ2lQE" }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >
+              <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text="My Marker"
+              />
+            </GoogleMapReact>
+          </div>
+          </div>
+        );
+      }
 }
 
 export default Maps;
